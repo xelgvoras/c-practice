ab246cf4a04949a492e0a06b7d2942a0fd7f4e2f
Xelg <xelgvoras@gmail.com>
2017-09-24 15:29:21 +0300

    #02 добавлен метод для расчета площади пересечения прямоугольников

--- Rectangles/RectanglesTask.cs
+++ Rectangles/RectanglesTask.cs
@@ -25,6 +25,13 @@ namespace Rectangles
 		// Площадь пересечения прямоугольников
 		public static int IntersectionSquare(Rectangle r1, Rectangle r2)
 		{
+            if (AreIntersected(r1, r2))
+            {
+                var height = Math.Max(r1.Top, r2.Top) - Math.Min(r1.Bottom, r2.Bottom);
+                var width = Math.Max(r1.Left, r2.Left) - Math.Min(r1.Right, r2.Right);
+                var square = height * width;
+                return square;
+            }
             return 0;
 		}
 
