e36fc3a36d2746ff870042e97638e5ac7c5fca07
Xelg <xelgvoras@gmail.com>
2017-09-24 12:05:22 +0300

    "Pluralize" добавлено склонение окончаний

--- Pluralize/PluralizeTask.cs
+++ Pluralize/PluralizeTask.cs
@@ -4,8 +4,18 @@
 	{
 		public static string PluralizeRubles(int count)
 		{
-			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
-			return "руб.";
+            // Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+            var modcount = count % 10;
+            var ending = "лей";
+            if (modcount == 1)
+            {
+                ending = "ль";
+            }
+            else if (modcount > 1 && modcount < 5)
+            {
+                ending = "ля";
+            }
+            return "руб" + ending;
 		}
 	}
 }
