5df556fc3fb53ad290b278966bdfddc2e99ce452
Xelg <xelgvoras@gmail.com>
2017-09-24 11:49:43 +0300

    Практика, добавлен проект "Pluralize"

---
+++ Pluralize/PluralizeTask.cs
@@ -0,0 +1,11 @@
+﻿namespace Pluralize
+{
+	public static class PluralizeTask
+	{
+		public static string PluralizeRubles(int count)
+		{
+			// Напишите функцию склонения слова "рублей" в зависимости от предшествующего числительного count.
+			return "руб.";
+		}
+	}
+}
