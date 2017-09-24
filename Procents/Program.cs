b5044604459a67f4330a571a80a98b370006a1f9
Xelg <xelgvoras@gmail.com>
2017-09-24 10:33:15 +0300

    bug fix проект "Procents"

--- Procents/Program.cs
+++ Procents/Program.cs
@@ -14,20 +14,20 @@ namespace Procents
             var inputLine = Console.ReadLine();
             var accumulation = Calculate(inputLine);
             Console.WriteLine(accumulation);
+        }
 
-            double Calculate(string input)
+        static private double Calculate(string input)
+        {
+            char delimiter = ' ';
+            string[] substrings = input.Split(delimiter);
+            var sum = double.Parse(substrings[0]);
+            var interestRate = double.Parse(substrings[1]) / 100;
+            var termOfDeposite = double.Parse(substrings[2]);
+            for (int i = 0; i < (12 * termOfDeposite); i++)
             {
-                char delimiter = ' ';
-                string[] substrings = input.Split(delimiter);
-                var sum = double.Parse(substrings[0]);
-                var interestRate = double.Parse(substrings[1]) / 100;
-                var termOfDeposite = double.Parse(substrings[2]);
-                for (int i = 0; i < (12 * termOfDeposite); i++)
-                {
-                    sum += (sum * interestRate) / 12;
-                }
-                return sum;
+                sum += (sum * interestRate) / 12;
             }
+            return sum;
         }
     }
 }
