25a37256d3c42b91fbf69c6bf6e3dfbd0a90d30d
Xelg <xelgvoras@gmail.com>
2017-09-30 19:09:06 +0300

    Добавлен алгоритм для прохождения пустого лабиринта.

--- Mazes/EmptyMazeTask.cs
+++ Mazes/EmptyMazeTask.cs
@@ -4,6 +4,20 @@
 	{
 		public static void MoveOut(Robot robot, int width, int height)
 		{
+            GoToX(robot, width);
+            GoToY(robot, height);
 		}
+
+        private static void GoToX(Robot robot, int coords)
+        {
+            while (robot.X != (coords - 2))
+                robot.MoveTo(Direction.Right);
+        }
+
+        private static void GoToY(Robot robot, int coords)
+        {
+            while (robot.Y != (coords - 2))
+                robot.MoveTo(Direction.Down);
+        }
 	}
 }
