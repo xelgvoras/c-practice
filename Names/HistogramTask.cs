2a06bd14f05bc5aed0ef6fa4e43c329609e0d57a
Xelg <xelgvoras@gmail.com>
2017-10-08 22:12:31 +0300

    Проект "Names", добавлен код, готовящий данные для построения гистограммы - количества людей в выборке c заданным именем в зависимости от числа

--- Names/HistogramTask.cs
+++ Names/HistogramTask.cs
@@ -8,7 +8,7 @@ namespace Names
 
 		public static HistogramData GetHistogramBirthsPerDay(NameData[] names, string name)
 		{
-			/*
+            /*
 			Напишите код, готовящий данные для построения гистограммы 
 			— количества людей в выборке c заданным именем в зависимости от числа (то есть номера дня в месяце) их рождения.
 			Не учитывайте тех, кто родился 1 числа любого месяца.
@@ -23,7 +23,23 @@ namespace Names
 			Пример подготовки данных для гистограммы смотри в файле HistogramSample.cs
 			*/
 
-			return new HistogramData(String.Format("Рождаемость людей с именем '{0}'", name), new string[0], new double[0]);
+            var minDay = 2;
+            var maxDay = int.MinValue;
+            foreach (var day in names)
+                maxDay = Math.Max(maxDay, day.BirthDate.Day);
+            var days = new string[maxDay - minDay + 1];
+            for (var i = 0; i < days.Length; i++)
+            {
+                days[i] = (i + minDay).ToString();
+            }
+            var birthCounts = new double[maxDay - minDay + 1];
+            foreach (var day in names)
+            {
+                if (day.Name == name && day.BirthDate.Day > 1)
+                    birthCounts[day.BirthDate.Day - minDay]++;
+            }
+
+			return new HistogramData(String.Format("Рождаемость людей с именем '{0}'", name), days, birthCounts);
 		}
 	}
 }
