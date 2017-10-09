83f8104acca36b722fe6b7b6b448c56b0b6231b9
Peter Chunin <pc@tract.ru>
2017-10-09 10:20:46 +0300

    Практика, добавлен проект "TextAnalysis"

---
+++ TextAnalysis/FrequencyAnalysisTask.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+	static class FrequencyAnalysisTask
+	{
+		/*
+		Постройте по тексту словарь наиболее вероятного продолжения текста.
+
+		Ключи этого словаря — это все слова, с которых начинается хотя бы одна биграмма исходного текста.
+		В качестве значения должно быть второе слово самой частой биграммы, начинающейся с ключа.
+		Если есть несколько биграмм с одинаковой частотой, то использовать лексикографически первую
+		(используйте способ сравнения Ordinal, например с помощью метода string.CompareOrdinal).
+		*/
+
+		public static Dictionary<string, string> GetMostFrequentNextWords(List<List<string>> text)
+		{
+			return new Dictionary<string, string>();
+		}
+
+	}
+}
---
+++ TextAnalysis/SentencesParserTask.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace TextAnalysis
+{
+	static class SentencesParserTask
+	{
+		public static readonly string[] StopWords = { "the", "and", "to", "a", "of", "in", "on", "at", "that", "as", "but", "with", "out", "for", "up", "one", "from", "into" };
+
+		/*
+		Разбейте файл с текстом на предложения и слова. 
+		Считайте, что слова могут состоять только из букв (используйте метод char.IsLetter) или символа апострофа ',
+		а предложения разделены одним из следующих символов .!?;:()
+		Удалите из файла слова, содержащиеся в массиве StopWords (частые незначащие слова при анализе текстов называют стоп-словами)
+		Метод должен возвращать список предложений, где каждое предложение — это список оставшихся слов в нижнем регистре.
+		*/
+		public static List<List<string>> ParseSentences(string text)
+		{
+			return new List<List<string>>();
+		}
+	}
+}
